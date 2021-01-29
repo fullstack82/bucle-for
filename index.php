543044614aeacb8f49cfd653899a1917789918f3
fullstack82 <fullstack8210@gmail.com>
2021-01-29 09:45:56 +0100

    tabla de multiplicar con el for

--- index.php
+++ index.php
@@ -1,17 +1,31 @@
 <?php
 
-
-/* Bucle for
-
-for(variable, condicion, actualizando el contador){
-
-    // Bloque de instrucciones
-}*/
-
-
+/*
 $resultado = 0;
 
-for ($i = 0; $i <= 100; $contador++){
-    $resultado = $resultado + $contador + $i;
+for($i = 0; $i <= 100; $i++){
+    $resultado = $resultado +$i;
 }
-echo "<h1> El resultado es: $resultado </h1>";
+echo "<h1>El resultado es: $resultado</h1>";
+
+*/
+
+$numero = 8;
+
+for($contador = 1; $contador <= 10; $contador++){
+    echo "$numero x $contador = ".($numero*$contador)."<br/>";
+
+}
+
+
+if(isset($_GET['numero'])){
+    $numero = (int)$_GET['numero'];
+}else{
+    $numero = 1;
+}
+echo "<h2>Tabla de multiplicar del numero $numero</h2>";
+
+
+for($contador = 1; $contador <= 10; $contador++){
+    echo "$numero x $contador = ".($numero*$contador)."<br/>";
+}
