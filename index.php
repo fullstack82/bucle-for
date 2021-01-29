72e4d8cda818cc9ff23f8c18adffa9c63945c4bc
fullstack82 <fullstack8210@gmail.com>
2021-01-29 09:55:36 +0100

    agregando break

--- index.php
+++ index.php
@@ -12,20 +12,25 @@ echo "<h1>El resultado es: $resultado</h1>";
 
 $numero = 8;
 
-for($contador = 1; $contador <= 10; $contador++){
-    echo "$numero x $contador = ".($numero*$contador)."<br/>";
-
+for ($contador = 1; $contador <= 10; $contador++) {
+    echo "$numero x $contador = " . ($numero * $contador) . "<br/>";
 }
 
 
-if(isset($_GET['numero'])){
+if (isset($_GET['numero'])) {
     $numero = (int)$_GET['numero'];
-}else{
+} else {
     $numero = 1;
 }
 echo "<h2>Tabla de multiplicar del numero $numero</h2>";
 
 
-for($contador = 1; $contador <= 10; $contador++){
-    echo "$numero x $contador = ".($numero*$contador)."<br/>";
+for ($contador = 1; $contador <= 10; $contador++) {
+
+        if($numero == 45){
+            echo "NO se puede mostrar la tabla";
+            break;    //Entra al bucle, comprueba algo y hace un break, sale del bucle inmediatamente
+        }
+
+    echo "$numero x $contador = " . ($numero * $contador) . "<br/>";
 }
